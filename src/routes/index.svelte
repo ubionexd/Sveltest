435667f8d550d79c6126ddce64ca957bec4f556b
UbiOne <ubionexd@gmail.com>
2022-05-29 16:01:06 +0200

    3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,9 +16,11 @@
 
 <section>
 	<div class="text-center text-xl font-bold select-none flex justify-center items-center h-screen">
-		<h1 class="text-[#ff3e00]">SvelteKit 
+		<div>
+			<h1 class="text-[#ff3e00]">SvelteKit</h1>
 			<h1 class="text-[#38bdf8]">with TailwindCSS</h1>
-				<p class="text-white mt-10 text-sm">Compiled, with no Virtual-DOM and Server-Side rendering.</p>
-					<p class="text-gray-400 text-sm">Feel free to use it on your project!</p></h1> 
+			<p class="text-white mt-10 text-sm">Compiled, with no Virtual-DOM and Server-Side rendering.</p>
+			<a class="text-gray-400 mt-1 text-sm hover:underline underline-offset-2" href="https://github.com/ubionexd/sveltest">Feel free to use it on your project!</a>
+		</div>
 	</div>
 </section>
